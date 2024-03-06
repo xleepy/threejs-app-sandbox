f7fb12011ed8a28bc5daf9934489cb26da597877
Radmir Gurkov <ragux0@gmail.com>
2024-03-06 23:08:01 +0100

    add all splats semantics

--- src/Luma.tsx
+++ src/Luma.tsx
@@ -23,7 +23,7 @@ export function Luma() {
   return (
     <lumaSplats
       ref={lumaRef}
-      semanticsMask={LumaSplatsSemantics.FOREGROUND}
+      semanticsMask={LumaSplatsSemantics.ALL}
       source="https://lumalabs.ai/capture/822bac8d-70d6-404e-aaae-f89f46672c67"
       position={[0, 0, 0]}
       scale={1}
