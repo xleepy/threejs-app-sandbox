cdc1ac4dc63fbe1a5822bdb5ce3964585f4ee171
Radmir Gurkov <ragux0@gmail.com>
2024-03-06 22:44:13 +0100

    make build happy

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,49 @@
-import { useCallback, useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+// import { useCallback, useRef, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Luma } from "./Luma";
 import { Stats } from "@react-three/drei";
 import "./App.css";
-import type {
-  BufferGeometry,
-  Material,
-  Mesh,
-  NormalBufferAttributes,
-  Object3DEventMap,
-  Vector3,
-} from "three";
-import { Luma } from "./Luma";
 
-type BoxProps = {
-  position?: Vector3;
-};
+// type BoxProps = {
+//   position?: Vector3;
+// };
 
-const Box = ({ position }: BoxProps) => {
-  const [isHovered, setHover] = useState(false);
+// const Box = ({ position }: BoxProps) => {
+//   const [isHovered, setHover] = useState(false);
 
-  const meshRef = useRef<Mesh<
-    BufferGeometry<NormalBufferAttributes>,
-    Material | Material[],
-    Object3DEventMap
-  > | null>(null);
+//   const meshRef = useRef<Mesh<
+//     BufferGeometry<NormalBufferAttributes>,
+//     Material | Material[],
+//     Object3DEventMap
+//   > | null>(null);
 
-  const pointerOver = useCallback(() => {
-    setHover(true);
-  }, []);
+//   const pointerOver = useCallback(() => {
+//     setHover(true);
+//   }, []);
 
-  const pointerOut = useCallback(() => {
-    setHover(false);
-  }, []);
+//   const pointerOut = useCallback(() => {
+//     setHover(false);
+//   }, []);
 
-  useFrame((_, delta) => {
-    const { current } = meshRef;
-    if (!current) {
-      return;
-    }
-    current.rotation.x += delta;
-  });
-  return (
-    <mesh
-      ref={meshRef}
-      position={position}
-      onPointerOver={pointerOver}
-      onPointerOut={pointerOut}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={isHovered ? "hotpink" : "orange"} />
-    </mesh>
-  );
-};
+//   useFrame((_, delta) => {
+//     const { current } = meshRef;
+//     if (!current) {
+//       return;
+//     }
+//     current.rotation.x += delta;
+//   });
+//   return (
+//     <mesh
+//       ref={meshRef}
+//       position={position}
+//       onPointerOver={pointerOver}
+//       onPointerOut={pointerOut}
+//     >
+//       <boxGeometry args={[1, 1, 1]} />
+//       <meshStandardMaterial color={isHovered ? "hotpink" : "orange"} />
+//     </mesh>
+//   );
+// };
 
 function App() {
   return (
@@ -66,8 +58,9 @@ function App() {
       />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
       <Box />
-      <Stats /> */}
+      */}
       <Luma />
+      <Stats />
     </Canvas>
   );
 }
